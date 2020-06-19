0a2f7e32852c4f501b85f6bd22012e9275a83ff4
stephenstee <steestephen3@gmail.com>
2020-06-19 12:01:44 +0000

    deploying

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var flash = require("connect-flash");
 var app = express()
 
 mongoose.connect("mongodb://localhost/bike_project",{useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect("mongodb+srv://stephen:stephen12051999@cluster0-tnebd.mongodb.net/<dbname>?retryWrites=true&w=majority",{useNewUrlParser:true, useUnifiedTopology:true});
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
